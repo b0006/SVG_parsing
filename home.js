ecde77adaf52287d023d234aa1a2b4478e4dd86d
b0006 <qsefthuken@gmail.com>
2018-02-28 11:57:04 +0700

    Изменена немного структура проекта

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let decoding = require('./decode');
+let decoding = require('./public/javascripts/decode');
 
 window.storage = {}; // для пространства имен, что бы много мусора в window не пихать
 window.storage.globalVar = decoding;
