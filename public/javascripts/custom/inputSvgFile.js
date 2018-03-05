7fc20f5b763358a819163733f1f6bd57f7b1c2a4
b0006 <qsefthuken@gmail.com>
2018-03-05 23:38:02 +0700

    На тестовом полигоне результат отличный

--- public/javascripts/custom/inputSvgFile.js
+++ public/javascripts/custom/inputSvgFile.js
@@ -1,54 +1,24 @@
 
 function handleFileSelect(evt) {
 
-    var files = evt.target.files; // FileList object
+     // var files = evt.target.files; // FileList object
 
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
+    d3.xml("../public/images/svgfile_main.svg", "image/svg+xml", function(xml) {
+        var importedNode = document.importNode(xml.documentElement, true);
+        d3.select("div#vis")
+            .each(function() {
+                this.appendChild(importedNode);
+            })
+    });
 
-        // Only process image files.
-        if (!f.type.match('image.*')) {
-            continue;
-        }
-
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-            return function(e) {
-                // Render thumbnail.
-
-                // удаляем div
-                $("div#svg_block").remove();
-
-                // создаем div
-                var div = $("<div id='svg_block'></div>");
-                $("#loaded_svgFile").append(div);
-
-                var object = '<object id="test" data="' + e.target.result + '" style="width: 70%; height: 70%;"></object>';
-                div.append(object);
-            };
-        })(f);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
-    }
 }
-
-function goToReadSvgFile() {
-
-    var parent = $('#test');
-    console.log(parent.content.textContent);
-}
-
 
 $('body').on('change','#svg_files', function(e){
+    e.preventDefault();
     handleFileSelect(e);
 });
 
-$('body').on('click','#go', function(e){
-    goToReadSvgFile();
-});
+
 
 
 
